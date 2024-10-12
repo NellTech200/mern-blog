c7df14386f3ba994a40def9805f605075e0b0b4a
Tony <gaonelleng@gmail.com>
2024-10-12 12:07:54 +0100

    Add signout functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,12 +5,30 @@ import { FaMoon, FaSun } from 'react-icons/fa';
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
+import { signoutSuccess } from '../redux/user/userSlice';
+
 
 export default function Header() {
     const path = useLocation().pathname;
     const dispatch = useDispatch();
     const { theme } = useSelector((state) => state.theme)
     const { currentUser } = useSelector(state => state.user)
+
+    const handleSignout = async () => {
+      try {
+          const res = await fetch('/api/user/signout', {
+              method: 'POST',
+          });
+          const data = await res.json();
+          if (!res.ok) {
+              console.log(data.message);
+          } else {
+              dispatch(signoutSuccess());
+          }
+      } catch (error) {
+          console.log(error);
+      }
+    }
   return (
     <Navbar className='border-b-2'>
       <Link to="/" className='self-Center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -45,7 +63,7 @@ export default function Header() {
                    <Dropdown.Item>Profile</Dropdown.Item>
                 </Link>
                 <Dropdown.Divider />
-                <Dropdown.Item>Sign Out</Dropdown.Item>
+                <Dropdown.Item onClick={handleSignout} >Sign Out</Dropdown.Item>
               </Dropdown>
             ) : (
               <Link to='/sign-in'>
