647758231939f25c3f8ff4d9231b8c291b3fccf2
Tony <gaonelleng@gmail.com>
2024-12-06 01:51:08 +0100

    complete create a post UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Dashboard from './pages/Dashboard'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import About from './pages/About'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import PrivateRoute from './components/PrivateRoute'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import About from './pages/About';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import PrivateRoute from './components/PrivateRoute';
+import CreatePost from './pages/CreatePost';
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 
 export default function App() {
   return (
@@ -22,6 +24,10 @@ export default function App() {
 
         <Route element={<PrivateRoute />} >
          <Route path="/dashboard" element={<Dashboard />} />
+        </Route> 
+
+        <Route element={<OnlyAdminPrivateRoute />} >
+         <Route path="/create-post" element={<CreatePost />} />
         </Route> 
         
         <Route path="/projects" element={<Projects />} />
