e194d8c3b42e28b7d1bc1022dfcf7994019eb795
Tony <gaonelleng@gmail.com>
2024-12-30 05:42:59 +0100

    show users to the admin dashboad

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
+import DashUsers from '../components/DashUsers';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -29,6 +30,9 @@ export default function Dashboard() {
 
       {/*posts */}
       {tab === 'posts' && <DashPosts />}
+
+      {/*posts */}
+      {tab === 'users' && <DashUsers />}
     </div>
   )
 }
