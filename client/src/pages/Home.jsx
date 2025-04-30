01cf19b73989baf8e3aedfc79f54c10ca726aa4e
Tony <gaonelleng@gmail.com>
2025-04-30 03:42:16 +0100

    complete about and project pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import CallToAction from '../components/CallToAction';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import PostCard from '../components/PostCard';
 
 export default function Home() {
@@ -12,36 +11,50 @@ export default function Home() {
       const res = await fetch('/api/post/getPosts');
       const data = await res.json();
       setPosts(data.posts);
-    }
+    };
     fetchPosts();
-  }, [])
+  }, []);
   return (
     <div>
-      <div className='flex flex-col gap-6 px-3 p-28 max-w-6xl mx-auto'>
-        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
-        <p className='text-gray-500 text-xs sm:text-sm'>Here you'll find a vareity of articles and tutorials on topics such as web developement, software engineering, and programming languages </p>
-        <Link to='/search' className='text-xs sm:text-sm tetxt-teal-500 font-bold hover:underline'>VIew All Posts </Link>
-      </div>
-      <div className='p-3 bg-amber-100 dark:bg-slate-700'>
-        <CallToAction />
+      <div className='flex flex-col gap-6 p-10  px-3 max-w-6xl mx-auto '>
+        <h1 className='text-3xl font-bold lg:text-6xl pt-10'>Welcome to my Blog</h1>
+        <p className='text-gray-500 text-xs sm:text-sm'>
+          Welcome to my blog! Here you'll find a wide range of articles,
+          tutorials, and resources designed to help you grow as a developer.
+          Whether you're interested in web development, software engineering,
+          programming languages, or best practices in the tech industry, there's
+          something here for everyone. Dive in and explore the content to expand
+          your knowledge and skills.
+        </p>
+        <Link
+          to='/search'
+          className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'
+        >
+          View all posts
+        </Link>
+        <div className='p-3 bg-amber-100 dark:bg-slate-700'>
+          <CallToAction />
+        </div>
       </div>
 
-      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-3'>
         {posts && posts.length > 0 && (
           <div className='flex flex-col gap-6'>
             <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
-            <div className='flex flex-wrap'>
+            <div className='flex flex-wrap gap-3'>
               {posts.map((post) => (
-                <PostCard key={post.id} post={post} />
+                <PostCard key={post._id} post={post} />
               ))}
             </div>
-            <Link to={'/search'} className='text-lg text-teal-500 hover:underline text-center'>
-              view all posts
+            <Link
+              to={'/search'}
+              className='text-lg text-teal-500 hover:underline text-center'
+            >
+              View all posts
             </Link>
           </div>
-        )
-        }
+        )}
       </div>
     </div>
-  )
+  );
 }
